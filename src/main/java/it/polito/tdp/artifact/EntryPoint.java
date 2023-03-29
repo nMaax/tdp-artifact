9a3ae7a151814d7b2077ed4f767894c1fe3e538c
nMaax <carli.massimiliano01@gmail.com>
2023-03-29 18:40:31 +0200

    Changes stage title to "Artifact"

--- src/main/java/it/polito/tdp/artifact/EntryPoint.java
+++ src/main/java/it/polito/tdp/artifact/EntryPoint.java
@@ -17,7 +17,7 @@ public class EntryPoint extends Application {
         scene.getStylesheets().add("/styles/Styles.css");
         scene.getRoot().setStyle("-fx-font-family: 'Arial'");
 
-        stage.setTitle("JavaFX and Maven");
+        stage.setTitle("Artifact");
         stage.setScene(scene);
         stage.show();
     }
