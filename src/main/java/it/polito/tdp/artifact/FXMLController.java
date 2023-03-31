239c6346db3ace1572691617650e47d6002ad0bd
nMaax <carli.massimiliano01@gmail.com>
2023-03-31 17:32:08 +0200

    Removed unused import

--- src/main/java/it/polito/tdp/artifact/FXMLController.java
+++ src/main/java/it/polito/tdp/artifact/FXMLController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.text.Font;
 
 public class FXMLController implements Initializable {
     
